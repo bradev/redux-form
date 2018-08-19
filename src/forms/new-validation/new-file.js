b9dc8ea0736f0afcf4e528a60dd69408e92b97aa
Imre Blauhut <imre.blauhut@gmail.com>
2018-08-19 23:04:40 +0300

    first new branch cmmit

---
+++ src/forms/new-validation/new-file.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NewComp extends React.Component {
+  state = {
+
+  }
+
+  render() {
+
+    return (
+     <div>
+
+     </div>
+    )
+  }
+
+}
+
+export default NewComp;
